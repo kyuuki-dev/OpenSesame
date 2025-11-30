f708f402ee1f6c52197276a3aaa8144e7cd594e2
kyuuki-dev <k.yuuki1905@gmail.com>
2025-11-30 14:38:20 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const APP_CONFIG = {
+  AUTH0_DOMAIN: "dev-tij17lcshqts0ymk.us.auth0.com",
+  CLIENT_ID: "RHct2tkctfaLIQo381Hd4twNvvTMLUnQ", // your Auth0 application's Client ID
+  API_BASE: "https://6wmks9fss1.execute-api.us-east-1.amazonaws.com"
+};
