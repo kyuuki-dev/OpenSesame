a3368f11b1478e221ac21bdf0319ac10a1cd3344
kyuuki-dev <k.yuuki1905@gmail.com>
2025-12-01 01:50:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const APP_CONFIG = {
   AUTH0_DOMAIN: "dev-tij17lcshqts0ymk.us.auth0.com",
   CLIENT_ID: "RHct2tkctfaLIQo381Hd4twNvvTMLUnQ", // your Auth0 application's Client ID
   API_BASE: "https://opensesamebe.onrender.com",
-  REDIRECT_URI: "https://kyuuki-dev.github.io/OpenSesame/"
+  REDIRECT_URI: "https://kyuuki-dev.github.io/OpenSesame/",
+  AUDIENCE: "https://opensesamebe.onrender.com"
 };
